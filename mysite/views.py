2db0b147b2e30da0287afb372328a012b169be96
root <root@localhost.localdomain>
2016-08-12 09:58:48 -0600

    modificando plantilla base y blog

---
+++ mysite/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.utils import timezone
+from django.db import models
+#from .models import Post
+
+# Create your views here.
+def base(request):
+   context = {}
+   return render(request,'base.html', context)
+
+def post_list(request):
+ posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+ return render(request, 'post_list.html', {'posts': posts})                                                              
